b1d3203a23f60e157a94e9ed77606de67c9fd122
Terrence Ho <terrenceho.books@gmail.com>
2017-04-10 20:24:18 -0700

    Separated BackProp from cost function, will use FMNI_CG from Scipy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import neuralnet
+import numpy as np
+import neuralnet as nn
+import scipy.io
+
+
 
 # parameters
 input_layer_size = 400
@@ -9,10 +13,18 @@ print("Loading Data")
 datafile = 'data.mat'
 mat = scipy.io.loadmat(datafile)
 X, y = mat['X'], mat['y'] #load in x and y
-X = np.insert(X, 0, 1 , axis = 1)
-m = X[0].shape() # size of X
+y[y==10] = 0 #set 10 in the dataset to 0
+X = np.insert(X, 0, 1 , axis = 1) #add in a bias unit of 0
+m = X[0].shape # size of X
 
 
 
 print("Placeholder for now, will show images and display data")
+
+#initializing random params for weights
+initialTheta1 = nn.randInitWeights(input_layer_size, hidden_layer_size)
+initialTheta2 = nn.reshapeParams(hidden_layer_size, num_labels)
+
+#unroll initialTheta1/initialTheta2
+
 
--- neuralnet.py
+++ neuralnet.py
@@ -9,63 +9,62 @@ import matplotlib.cm as cm
 import random
 import itertools
 
-def __init__(self):
-
 
 def nnCostFunction(nn_params, inputLayerSize, hiddenLayerSize, numLabels, X, y, lambdaVar):
 
-	# Flattens the Theta1, Theta2 from nn_params.
-	# m = size(X,1)
-	# Return Theta1Grad, Theta2Grad, and J the cost
+    # Flattens the Theta1, Theta2 from nn_params.
+    # m = size(X,1)
+    # Return Theta1Grad, Theta2Grad, and J the cost
 
-	# create eye matrix
-	# set y to y
+    # create eye matrix
+    # set y to y
 
-	# feed forward through the neural network
+    # feed forward through the neural network
 
-	Theta1 = np.reshape(nn_params, (hiddenLayerSize, inputLayerSize+1))
-	Theta2 = np.reshape(nn_params, (numLabels, hiddenLayerSize+1))
-	m = X.shape[0]
+    Theta1 = np.reshape(nn_params, (hiddenLayerSize, inputLayerSize+1))
+    Theta2 = np.reshape(nn_params, (numLabels, hiddenLayerSize+1))
+    m = X.shape[0]
 
-	J = 0
-	Theta1Grad = np.zeroes(np.shape(Theta1))
-	Theta2Grad = np.zeroes(np.shape(Theta2))
+    J = 0
+    Theta1Grad = np.zeros(np.shape(Theta1))
+    Theta2Grad = np.zeros(np.shape(Theta2))
 
-        rows, cols = y.shape
-        y1 = np.zeros((rows, numLabels))
-        for i in range(0, rows):
-            y1[i, y[i]] = 1
+    rows, cols = y.shape
+    y1 = np.zeros((rows, numLabels))
+    for i in range(0, rows):
+        y1[i, y[i]] = 1
 
-	a1 = np.concatenate((np.ones((m,1)), X),1)
-	z2 = a1 * Theta1.T
-	a2 = np.concatenate((np.ones((m,1)), sigmoid(z2)),1)
-	z3 = a2 * Theta.T
-	a3 = sigmoid(z3)
-	h = a3
+    a1 = np.concatenate((np.ones((m,1)), X),1)
+    z2 = a1 * Theta1.T
+    a2 = np.concatenate((np.ones((m,1)), sigmoid(z2)),1)
+    z3 = a2 * Theta2.T
+    a3 = sigmoid(z3)
+    h = a3
 
-	Theta1Reg = np.sum(np.sum(Theta1[:,1:]) ** 2)
-	Theta2Reg = np.sum(np.sum(Theta2[:,1:]) ** 2)
+    Theta1Reg = np.sum(np.sum(Theta1[:,1:]) ** 2)
+    Theta2Reg = np.sum(np.sum(Theta2[:,1:]) ** 2)
 
-	r = (lambdaVar/(2 * m)) * (Theta1Reg + Theta2Reg)
+def backPropagate(mythetas_flat, myx_flat, mmy, lambda):
+    r = (lambdaVar/(2 * m)) * (Theta1Reg + Theta2Reg)
 
-	J = (1/m) * np.sum(np.sum((-y1) * log(h) - (1-y1) * log(1-h))) + r
+    J = (1/m) * np.sum(np.sum((-y1) * log(h) - (1-y1) * log(1-h))) + r
 
-	d3 = a3 - y1;
-	d2 = sigmoidGradient(z2) * (d3 * Theta2[:,1:])
+    d3 = a3 - y1;
+    d2 = sigmoidGradient(z2) * (d3 * Theta2[:,1:])
 
-	Delta1 = d2.T * a1
-	Delta2 = d3.T * a2
+    Delta1 = d2.T * a1
+    Delta2 = d3.T * a2
 
-	Theta1Grad = (1/m) * Delta1
-	Theta2Grad = (1/m) * Delta2
+    Theta1Grad = (1/m) * Delta1
+    Theta2Grad = (1/m) * Delta2
 
 
-	#CHECK
-	Theta1[:,0] = 0
-	Theta2[:,0] = 0
+    #CHECK
+    Theta1[:,0] = 0
+    Theta2[:,0] = 0
 
-	Theta1Grad = Theta1Grad + ((lambdaVar/m) * Theta1)
-	Theta2Grad = Theta2Grad * ((lambdaVar/m) * Theta2)
+    Theta1Grad = Theta1Grad + ((lambdaVar/m) * Theta1)
+    Theta2Grad = Theta2Grad * ((lambdaVar/m) * Theta2)
 
 def randInitWeights(layerIn, layerOut):
     epsilon_init = 0.12
@@ -78,23 +77,20 @@ def sigmoidGradient(z):
     return sigmoid(z) * (1-sigmoid(z))
 
 def predict(theta1, theta2):
-
+    print("Predict")
 
 # Helper Functions.  These all flatten various thetas
 def flattenParams(thetas_list):
-    """
-    Hand this function a list of theta matrices, and it will flatten it
-    into one long (n,1) shaped numpy array
-    """
+    # Hand this function a list of theta matrices, and it will flatten it into one long (n,1) shaped numpy array
+
     flattened_list = [ mytheta.flatten() for mytheta in thetas_list ]
-    combined =
-    list(itertools.chain.from_iterable(flattened_list))
-    assert len(combined) == (input_layer_size+1)*hidden_layer_size + \(hidden_layer_size+1)*output_layer_size
+    combined = list(itertools.chain.from_iterable(flattened_list))
+    assert len(combined) == (input_layer_size+1)*hidden_layer_size + (hidden_layer_size+1)*output_layer_size
     return np.array(combined).reshape((len(combined),1))
 
 def reshapeParams(flattened_array):
-    theta1 = flattened_array[:(input_layer_size+1)*hidden_layer_size] \.reshape((hidden_layer_size,input_layer_size+1))
-    theta2 = flattened_array[(input_layer_size+1)*hidden_layer_size:] \.reshape((output_layer_size,hidden_layer_size+1))
+    theta1 = flattened_array[:(input_layer_size+1)*hidden_layer_size].reshape((hidden_layer_size,input_layer_size+1))
+    theta2 = flattened_array[(input_layer_size+1)*hidden_layer_size:].reshape((output_layer_size,hidden_layer_size+1))
     return [ theta1, theta2 ]
 
 def flattenX(myX):
@@ -102,3 +98,5 @@ def flattenX(myX):
 
 def reshapeX(flattenedX):
     return np.array(flattenedX).reshape((n_training_samples,input_layer_size+1))
+
+
