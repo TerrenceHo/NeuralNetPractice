3d71650dcc05a2038dafeb199a1c43eb6a1b0da9
Terrence Ho <terrenceho.books@gmail.com>
2017-04-28 16:46:52 -0700

    Loading from mat file works

--- predict.py
+++ predict.py
@@ -1,14 +1,25 @@
 from draw import Paint
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io import loadmat
+from neuralnet import forwardProp
 
-drawing = Paint()
-im_arr = drawing.get_digit()
+def main():
+    # initialize Paint class
+    drawing = Paint()
+    im_arr = drawing.get_digit()
 
-plt.axis('off')
-plt.imshow(im_arr, cmap=plt.cm.gray_r, interpolation='nearest')
-plt.show()
+    # retrieve weights from neuralnet.  Looks for 'Thetas.mat'
+    data = loadmat('Thetas.mat')
+    Theta1 = data['Theta1']
+    Theta2 = data['Theta2']
 
-im_vec = im_arr.reshape(1, -1)
+    plt.axis('off')
+    plt.imshow(im_arr, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.show()
+
+    im_vec = im_arr.reshape(1, -1)
 
 
+if __name__ == '__main__':
+    main()
